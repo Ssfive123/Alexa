931ef9327ea3391ac6bbe5fb5a10437e2b39d4a4
Ꮴᴀᴍᴘɪʀᴇ <103425355+TG-V4MP1R3@users.noreply.github.com>
2022-05-01 11:50:36 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Aadhi000/Ajax.git /Ajax
+  git clone https://github.com/TG-V4MP1R3/Alexa.git /Alexa
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Ajax
+  git clone $UPSTREAM_REPO /Alexa
 fi
 cd /Ajax
 pip3 install -U -r requirements.txt
-echo "Starting ᗩᒍᗩ᙭....🔥"
+echo "Starting 𝔸𝕃𝔼𝕏𝔸....🔥"
 python3 bot.py
